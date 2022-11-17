5de390b96465f3051a445b731a71a1781625a435
Cage03 <timmermansstijn1@gmail.com>
2022-11-17 13:38:32 +0100

    generateNotes(), generateNoteData(), Set States

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 ﻿import './App.css';
 import ChordsPage from './Pages/ChordsPage';
 import ScalesPage from "./Pages/ScalesPage";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        <ChordsPage/>
-        <br/><br/><br/>
-        <ScalesPage/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/Chords" element={<ChordsPage/>}/>
+                <Route path="/Scales" element={<ScalesPage/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
