3c1b71c64b0c9b8d2c1d5b23f45810011264a4fd
Cage03 <timmermansstijn1@gmail.com>
2022-11-17 10:32:01 +0100

    Fixed Previous Whoopsie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import ChordsPage from './Pages/ChordsPage';
+import ScalesPage from "./Pages/ScalesPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ChordsPage/>
+        <br/><br/><br/>
+        <ScalesPage/>
     </div>
   );
 }
